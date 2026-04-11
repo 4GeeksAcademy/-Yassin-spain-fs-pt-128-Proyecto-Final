374ecd4f39e0ae6d94cefde69dcccac6353723d4
winstylish <program.bussiness@gmail.com>
2026-04-11 15:02:01 +0200

    fix: corregir wsgi.py para que gunicorn encuentre la app en src/

--- src/wsgi.py
+++ src/wsgi.py
@@ -1,5 +1,11 @@
 # This file was created to run the application on heroku using gunicorn.
 # Read more about it here: https://devcenter.heroku.com/articles/python-gunicorn
+# Este archivo se usa para arrancar la aplicación con gunicorn en producción
+import sys
+import os
+
+
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'src'))
 
 from app import app as application
 
